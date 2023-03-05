def154e706d1ada9cc92ecc1b56e2d609b94e51b
Tom Forbes <tom@tomforb.es>
2023-03-05 23:15:44 +0000

    Try this

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,36 @@
 const fs = require('fs');
 
 module.exports = async ({github, fetch}) => {
-    let response = await github.rest.repos.listForOrg({
-        org: "pypi-data",
-        sort: "full_name",
-    });
+    let x = await github.rest.secretScanning.listAlertsForOrg({
+  org: "pypi-data",
+});
+    console.log(x);
+    // let response = await github.rest.repos.listForOrg({
+    //     org: "pypi-data",
+    //     sort: "full_name",
+    // });
 
-    let repo_names = response.data.map(r => r.full_name).filter(name => name.startsWith("pypi-data/pypi-code-")).map(name => name.split('/')[1]);
+    // let repo_names = response.data.map(r => r.full_name).filter(name => name.startsWith("pypi-data/pypi-code-")).map(name => name.split('/')[1]);
 
-    let indexes = [];
-    for (const idx in repo_names) {
-        let name = repo_names[idx];
-        let api_response = await github.rest.repos.getContent({
-            owner: "pypi-data",
-            repo: name,
-            path: "index.json",
-        });
-        let response = await fetch(api_response.data.download_url);
-        let content = await response.json();
-        console.log(name);
-        // console.log(content);
-        let count = Object.values(content.entries).reduce((a, v) => a + v.length, 0);
-        let output = {
-            url: content.url,
-            earliest_release: content.earliest_release,
-            latest_release: content.latest_release,
-            count
-        }
-        console.log(output);
-    }
+    // let indexes = [];
+    // for (const idx in repo_names) {
+    //     let name = repo_names[idx];
+    //     let api_response = await github.rest.repos.getContent({
+    //         owner: "pypi-data",
+    //         repo: name,
+    //         path: "index.json",
+    //     });
+    //     let response = await fetch(api_response.data.download_url);
+    //     let content = await response.json();
+    //     console.log(name);
+    //     // console.log(content);
+    //     let count = Object.values(content.entries).reduce((a, v) => a + v.length, 0);
+    //     let output = {
+    //         url: content.url,
+    //         earliest_release: content.earliest_release,
+    //         latest_release: content.latest_release,
+    //         count
+    //     }
+    //     console.log(output);
+    // }
 }
